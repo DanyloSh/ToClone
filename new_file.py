91733a7f3ff087a2f3ecf628831f61cf1a891c74
DanyloSh <danylo.shuflita@edu.turkuamk.fi>
2024-09-26 11:47:59 +0300

    commit

--- new_file.py
+++ new_file.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+print("We made some changes!!!")
+print("new text!")
 print("We made some changes!!!!!!")
 print("new text")
 print("some new text!")
-=======
-print("We made some changes!!!")
-print("new text!")
->>>>>>> test_branch
